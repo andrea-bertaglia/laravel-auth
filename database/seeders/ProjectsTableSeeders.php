ffa5e3d6e3030bcef8f8fcf62d0a9063a5657132
andrea-bertaglia <andreabertaglia@gmail.com>
2024-06-19 19:08:36 +0200

    feat: create project model and faker seeders one item

---
+++ database/seeders/ProjectsTableSeeders.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+class ProjectsTableSeeders extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        $newProject = new Project();
+        $newProject->title = $faker->sentence(5);
+        $newProject->description = $faker->text(500);
+        $newProject->link = $faker->url;
+        $newProject->thumb = $faker->imageUrl(360, 360, 'placeholder', true,);
+        $newProject->technologies = $faker->randomElement(['HTML', 'CSS', 'JavaScript', 'VUE.js', 'PHP', 'Laravel']);
+        $newProject->slug = Str::slug($newProject->title);
+        // dd($newProject);
+        $newProject->save();
+    }
+}
